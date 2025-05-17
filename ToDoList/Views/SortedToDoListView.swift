4f9fdfb1449fc90dab00d4e5e1d572d09d57a307
Laura Steiner <steiner.laura@gmail.com>
2025-05-16 18:36:59 -0700

    Add subView that uses the sort selection to sort the query.

--- ToDoList/Views/SortedToDoListView.swift
+++ ToDoList/Views/SortedToDoListView.swift
@@ -6,13 +6,90 @@
 //
 
 import SwiftUI
+import SwiftData
 
 struct SortedToDoListView: View {
+	@Query var todos: [ToDo]
+	@Environment(\.modelContext) var modelContext
+	let sortSelection: SortOption
+	
+	init( sortSelection: SortOption){
+		self.sortSelection = sortSelection
+		
+		switch self.sortSelection {
+			case .asEntered:
+				_todos = Query()
+			case .alphabetical:
+				_todos = Query(sort: \.todo, order: .forward)
+			case .chronological:
+				_todos = Query(sort: \.dueDate, animation: .default)
+			case .completed:
+				_todos = Query(filter: #Predicate { $0.isCompleted == false })
+		}
+	}
+	
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+		List {
+			ForEach(todos){ todo in
+				VStack(alignment: .leading) {
+					HStack {
+						Image(systemName:  todo.isCompleted ? "checkmark.rectangle" : "rectangle")
+							.onTapGesture {ture in
+								
+								todo.isCompleted.toggle()
+								
+								guard let _ = try? modelContext.save() else {
+									print("ERROR: save after toggle on ToDoListView did not work.")
+									return
+								}
+							}
+						NavigationLink {
+							DetailView(todo: todo)
+						} label: {
+							Text(todo.todo)
+						}
+						.swipeActions {
+							Button("Delete", role: .destructive) {
+								modelContext.delete(todo)
+								// The following updates the db right away
+								guard let _ = try? modelContext.save() else {
+									print("ERROR: save after delete on ToDoListView did not work.")
+									return
+								}
+							}
+						}
+					}
+					.font(.title2)
+					HStack{
+						if todo.reminderIsOn {
+							// TODO: change from hack of adding an invisible rectangle to a better way to align second row with first
+							Image(systemName:   "rectangle")
+								.font(.title2)
+								.opacity(0)
+							Text(todo.dueDate.formatted(date: .abbreviated	, time: .shortened))
+								.foregroundStyle(.secondary)
+							Image(systemName: "calendar")
+								.symbolRenderingMode(.multicolor)
+						}
+					}
+				}
+			}
+			/*
+			 .onDelete{ indexSet in
+			 indexSet.forEach { index in
+			 modelContext.delete(todos[index])
+			 guard let _ = try? modelContext.save() else {
+			 print("ERROR: save after delete on ToDoListView did not work.")
+			 return
+			 }
+			 }
+			 }
+			 */
+		}
+		.listStyle(.plain)
     }
 }
 
 #Preview {
-    SortedToDoListView()
+	SortedToDoListView( sortSelection: .alphabetical)
 }
